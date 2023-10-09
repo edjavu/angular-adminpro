aa7681af84181beeeb00fd7e576c7a0c5129eaf5
Eduardo Veintimilla <eduardojavy@gmail.com>
2023-10-09 10:59:17 -0500

    Fin seccion 8

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { Subscription, filter, map } from 'rxjs';
+
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,6 +9,30 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy {
+
+  public titulo?: string;
+  public tituloSubs$?: Subscription;
+
+  constructor(private router: Router){
+    this.tituloSubs$ = this.getArgumentoRuta()
+                        .subscribe(data => {
+                          this.titulo = data['titulo']
+                          document.title = `AdminPro - ${this.titulo!}`;
+                        });
+  }
+
+  ngOnDestroy(): void {
+    this.tituloSubs$?.unsubscribe();
+  }
+
+  getArgumentoRuta(){
+    return this.router.events
+      .pipe(
+        filter( (event: any) => event instanceof ActivationEnd),
+        filter( (event: ActivationEnd) => event.snapshot.firstChild === null),
+        map( (event: ActivationEnd) => event.snapshot.data)
+      );
+  }
 
 }
